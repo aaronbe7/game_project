beef3dca755f60ee26a82a2a861d69b2f7c35797
Aaron Bellach <aaronbellach@Aarons-MacBook-Pro.local>
2020-11-02 13:10:44 -0800

    Cached Elements

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*----- constants -----*/
+
+const slotImages = {
+    coin: 'imgs/number_1.png',
+    dollar: 'imgs/number_2.png',
+    seven: 'imgs/number_3.png',
+    key: 'imgs/number_4.png',
+}
+
+const min_same = 3;
+
+/*----- app's state (variables) -----*/
+
+let slots;
+let winner;
+
+/*----- cached element references -----*/
+
+const slotsEl = {
+    slot_1: {
+        imgEl: document.querySelector('slot1-result img')
+    }
+}
+
+/*----- event listeners -----*/
+
+document.querySelector('button').addEventListener('click', playGame);
+
+init();
+
+function init(){
+
+}
+
+/*----- functions -----*/
+
